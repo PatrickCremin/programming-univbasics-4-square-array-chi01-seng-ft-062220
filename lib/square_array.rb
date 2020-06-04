7bbcd7afb4ca417c13c3523953c8b35e33136b66
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-04 00:12:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,17 @@
 def square_array(array)
 i = 0
 arr = []
-while (i < array.length) do
+while (i < array.count) do
 arr << num ** 2
 i += 1
 end
 arr = []
+end
+
+def square_array(array)
+  arr = []
+  array.each do |num|
+arr << num ** 2
+end
+arr
 end
